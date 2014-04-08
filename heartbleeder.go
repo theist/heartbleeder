76df144e613e720cd4d84806f949b76d952add61
Carlos <theistian@gmail.com>
2014-04-08 17:39:08 +0200

    Stupid change to follow nagios plugin specs
    http://nagios.sourceforge.net/docs/3_0/pluginapi.html

--- heartbleeder.go
+++ heartbleeder.go
@@ -21,18 +21,18 @@ func main() {
 	}
 	c, err := tls.Dial("tcp", host, &tls.Config{InsecureSkipVerify: true})
 	if err != nil {
-		log.Printf("Error connecting to %s: %s\n", host, err)
-		os.Exit(2)
+		log.Printf("UNKNOWN Error connecting to %s: %s\n", host, err)
+		os.Exit(3)
 	}
 
 	err = c.WriteHeartbeat(32, nil)
 	if err == tls.ErrNoHeartbeat {
-		fmt.Printf("SECURE - %s does not have the heartbeat extension enabled\n", host)
+		fmt.Printf("OK - %s does not have the heartbeat extension enabled\n", host)
 		os.Exit(0)
 	}
 	if err != nil {
 		fmt.Println("UNKNOWN - Heartbeat enabled, but there was an error writing the payload:", err)
-		os.Exit(2)
+		os.Exit(3)
 	}
 
 	readErr := make(chan error)
@@ -44,12 +44,12 @@ func main() {
 	select {
 	case err := <-readErr:
 		if err == nil {
-			fmt.Printf("VULNERABLE - %s has the heartbeat extension enabled and is vulnerable to CVE-2014-0160\n", host)
-			os.Exit(1)
+			fmt.Printf("CRITICAL - %s has the heartbeat extension enabled and is vulnerable to CVE-2014-0160\n", host)
+			os.Exit(2)
 		}
-		fmt.Printf("SECURE - %s has heartbeat extension enabled but is not vulnerable\n", host)
+		fmt.Printf("OK - %s has heartbeat extension enabled but is not vulnerable\n", host)
 		fmt.Printf("This error happened while reading the response to the malformed heartbeat (almost certainly a good thing): %q\n", err)
 	case <-time.After(10 * time.Second):
-		fmt.Printf("SECURE - %s has the heartbeat extension enabled, but timed out after a malformed heartbeat (this likely means that it is not vulnerable)\n", host)
+		fmt.Printf("OK - %s has the heartbeat extension enabled, but timed out after a malformed heartbeat (this likely means that it is not vulnerable)\n", host)
 	}
 }
